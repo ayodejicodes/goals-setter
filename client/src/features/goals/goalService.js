fc9b178aeb84b3cd84a1e4be5070a49b72779b60
Ayodeji <creative.dee1@gmail.com>
2023-05-02 04:17:09 +0200

    deployment

--- client/src/features/goals/goalService.js
+++ client/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/goals/";
+const API_URL = "https://goals-setter-api.onrender.com/api/goals/";
 
 const createGoal = async (goalData, token) => {
   const config = {
